c0c77af2c52da548bd225da85b32911352eb6bde
Dominique VASSARD <dominique.vassard@gmail.com>
2020-04-02 18:01:51 +0200

    <Node> Moive delete/2 to Node.Schema

--- lib/neo4jex/repo/node/schema.ex
+++ lib/neo4jex/repo/node/schema.ex
@@ -1,5 +1,5 @@
 defmodule Neo4jex.Repo.Node.Schema do
-  alias Neo4jex.Query.{Builder, Planner}
+  alias Neo4jex.Query.{Builder, Helper, Planner}
 
   @spec create(Neo4jex.Repo.t(), Neo4jex.Schema.Node.t(), Neo4jex.Repo.Schema.create_options()) ::
           {:ok, Neo4jex.Schema.Node.t()}
@@ -56,5 +56,44 @@ defmodule Neo4jex.Repo.Node.Schema do
     {:ok, [%{"n" => created_node}]} = Planner.query(repo, cql, Map.merge(params, sets.params))
 
     {:ok, Map.put(data, :__id__, created_node.id)}
+  end
+
+  @spec delete(Neo4jex.Repo.t(), Ecto.Changeset.t()) :: {:ok, Neo4jex.Schema.Node.t()}
+  def delete(repo, %Ecto.Changeset{valid?: true} = changeset) do
+    data =
+      changeset
+      |> Map.put(:changes, %{})
+      |> Neo4jex.Changeset.apply_changes()
+
+    queryable = data.__struct__
+
+    node_to_del = %Builder.NodeExpr{
+      variable: "n",
+      labels: [queryable.__schema__(:primary_label)]
+    }
+
+    merge_keys_data = Helper.build_where_from_merge_keys(node_to_del, queryable, data)
+
+    {statement, params} =
+      Builder.new(:delete)
+      |> Builder.match([node_to_del])
+      |> Builder.delete([node_to_del])
+      |> Builder.where(merge_keys_data.where)
+      |> Builder.params(merge_keys_data.params)
+      |> Builder.to_string()
+
+    {:ok, %{stats: stats}} = Planner.query(repo, statement, params, with_stats: true)
+
+    case stats do
+      %{"nodes-deleted" => 1} ->
+        {:ok, data}
+
+      [] ->
+        raise Neo4jex.DeletionError, queryable: queryable, data: data
+    end
+  end
+
+  def delete(_, changeset) do
+    {:error, changeset}
   end
 end
