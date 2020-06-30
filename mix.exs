469639738276f4b1e5f4b60bb8fdd586976aeafe
Dominique VASSARD <dominique.vassard@gmail.com>
2020-06-30 17:44:02 +0200

    Add coeveralls + remove useless file

--- mix.exs
+++ mix.exs
@@ -14,7 +14,14 @@ defmodule Seraph.MixProject do
       start_permanent: Mix.env() == :prod,
       docs: docs(),
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -98,9 +105,11 @@ defmodule Seraph.MixProject do
       {:ecto, "~>3.2"},
       {:uuid, "~> 1.1"},
       {:inflex, "~> 2.0.0"},
+      # dev and test only deps
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 end
